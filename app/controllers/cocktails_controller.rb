d3157f98802c7a81adacf94cf61cf3781e225d3f
Erik Djamgarian <djamgarian0702@gmail.com>
2018-05-17 22:56:58 +0100

    nearly

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,2 +1,40 @@
 class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: [:show, :edit, :update]
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def edit
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+
+  private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
 end
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,42 @@
 class DosesController < ApplicationController
+  before_action :set_dose, only: [:show, :edit, :update]
+
+
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    if @dose.save
+      redirect_to doses_path
+    else
+      render :new
+    end
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def destroy
+    @dose.destroy
+    redirect_to doses_path
+  end
+
+  private
+
+  def set_dose
+    @dose = Dose.find(params[:id])
+  end
+
+  def dose_params
+    params.require(:dose).permit(:description)
+  end
 end
